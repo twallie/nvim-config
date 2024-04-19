d3943aee4c1c99a0d1041b56cc3ec6de63502499
twallie <thomaswallsmith@gmail.com>
2024-04-19 10:28:27 -0400

    feat: no more italics!

--- lua/twallie/lazy/colors.lua
+++ lua/twallie/lazy/colors.lua
@@ -3,7 +3,23 @@ return {
 		"folke/tokyonight.nvim",
 		lazy = true,
 		opts = { style = "night" },
-	},
+
+		config = function()
+			require('tokyonight').setup{
+				styles = {
+					-- Style to be applied to different syntax groups
+					-- Value is any valid attr-list value for `:help nvim_set_hl`
+					comments = { italic = true },
+					keywords = { italic = false },
+					functions = {},
+					variables = {},
+					-- Background styles. Can be "dark", "transparent" or "normal"
+					sidebars = "dark", -- style for sidebars, see below
+					floats = "dark", -- style for floating windows
+				},
+			}
+		end
+	},	
 
 
 	-- so if this isnt here no colorscheme is loaded wtf terrible awful terrible
