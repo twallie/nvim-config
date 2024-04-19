157a2b6a2d9f8cd36a6f0acfe0a412edf3cf35a4
twallie <thomaswallsmith@gmail.com>
2024-04-19 03:37:05 -0400

    feat: added remap for easy opening of terminal

--- lua/twallie/remap.lua
+++ lua/twallie/remap.lua
@@ -3,14 +3,17 @@ local TAB_SIZE = 4
 vim.g.mapleader = " "
 
 -- show project tree on " pv"
-vim.keymap.set("n", "<leader>pv", ":Neotree<CR>")
+vim.keymap.set("n", "<leader>pv", ":Neotree reveal<CR>")
 
 -- terminal shiz
 vim.keymap.set("t", "<esc>", "<C-\\><C-n>") -- allow for esc to work normally while in terminal
-vim.keymap.set("n", "<leader>tt", ":belowright 10 split | term<CR>")
+vim.keymap.set("n", "<leader>tt", ":belowright 10 split | term<CR>") -- allow for easy terminal opening
 
 -- getting my tabs right sheeeesh
 local set = vim.opt -- set options
 set.tabstop = TAB_SIZE
 set.softtabstop = TAB_SIZE
 set.shiftwidth = TAB_SIZE
+
+-- set relative line numbers
+set.relativenumber = true
