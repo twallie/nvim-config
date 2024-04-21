515be192427f9335197954900791a63e92db9e08
twallie <thomaswallsmith@gmail.com>
2024-04-21 15:02:40 -0400

    feat: shorthand for getting to start/end of line

--- lua/twallie/remap.lua
+++ lua/twallie/remap.lua
@@ -2,12 +2,35 @@
 local TAB_SIZE = 4
 vim.g.mapleader = " "
 
+
+----------------
+-- NAVIGATION --
+----------------
+
 -- show project tree on " pv"
 vim.keymap.set("n", "<leader>pv", ":Neotree reveal<CR>")
 
--- terminal shiz
-vim.keymap.set("t", "<esc>", "<C-\\><C-n>") -- allow for esc to work normally while in terminal
-vim.keymap.set("n", "<leader>tt", ":belowright 10 split | term<CR>") -- allow for easy terminal opening
+-- go to first char in line
+vim.keymap.set("n", "<C-H>", "^")
+
+-- go to end of line
+vim.keymap.set("n", "<C-L>", "$")
+
+----------------
+----------------
+
+----------------
+--- TERMINAL ---
+----------------
+
+-- allow for esc to work normally while in terminal
+vim.keymap.set("t", "<esc>", "<C-\\><C-n>")
+
+-- allow for easy terminal opening
+vim.keymap.set("n", "<leader>tt", ":belowright 10 split | term<CR>") 
+
+----------------
+----------------
 
 -- getting my tabs right sheeeesh
 local set = vim.opt -- set options
